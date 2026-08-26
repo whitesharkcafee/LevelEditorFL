0fee65ad075c65f5e0c700d6307e90c9bd3321c2
whitesharkcafe <305790387+whitesharkcafee@users.noreply.github.com>
2026-08-26 14:07:52 +0300

    Blocked REFRESHING_LOCALIZATION for Editor and Playmode.

---
+++ Playmode/Patches/RefreshLocalizationPatches.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using FS_LevelEditor.Editor;
+
+namespace FS_LevelEditor.Playmode.Patches
+{
+    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.RefreshLocalizationFromCloud))]
+    public static class RefreshLocalizationPatches
+    {
+        public static bool Prefix()
+        {
+            // if it's either Editor or Playmode, ignore Shift+L download.
+            if(EditorController.Instance != null || PlayModeController.Instance != null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(InGameUIManager), nameof(InGameUIManager.ShowNotification))]
+    public static class InGameUIRefreshLocalizationPatches
+    {
+        public static bool Prefix(InGameUIManager.NotificationType _type)
+        {
+            if((EditorController.Instance != null || PlayModeController.Instance != null) && _type == InGameUIManager.NotificationType.REFRESHING_LOCALIZATION)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
